72bde19387954fbfbbbdfaec47db57108034eabd
pankaj <pankajsemwalpankaj123@gmail.com>
2023-05-05 11:28:53 +0530

    installed cron for scheduling reminders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express =require('express')
  const bodyParser= require('body-parser')
 const { sendBasicEmail } = require('./services/email-service')
 
+var cron = require('node-cron');
+
+
  const {PORT} =require('./config/serverConfig')
  const setupAndStartServer =()=>{
      const app= express()
@@ -9,7 +12,12 @@ const { sendBasicEmail } = require('./services/email-service')
      app.use(bodyParser.urlencoded({extended:true}))
 
      app.listen(PORT,()=>{
-        console.log('server started on ',PORT);
+
+        // console.log('server started on ',PORT);
+        // cron.schedule('*/1 * * * *', () => {
+        //     console.log('running a task every  minute');
+        //   });
+        
         // sendBasicEmail(
         //     'support@admin.com',
         //     'pankajsemwalpankaj123@gmail.com',
